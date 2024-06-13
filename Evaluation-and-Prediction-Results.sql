f83bea2c3e76dd2be68752ae4a58c442a93d8db7
Xuân Linh <xuanlinhnguyen99@gmail.com>
2024-06-13 16:12:58 +0700

    Update Evaluation-and-Prediction-Results.sql

--- Evaluation-and-Prediction-Results.sql
+++ Evaluation-and-Prediction-Results.sql
@@ -13,3 +13,31 @@ FROM ML.EVALUATE (
 
 
 # Predict the duration of a bike rental
+
+  SELECT tripduration AS actual_duration,
+      predicted_tripduration AS predicted_duration,
+      ABS(tripduration - predicted_tripduration) AS difference_in_min
+FROM ML.PREDICT 
+  (MODEL `01_nyc_bike_sharing.trip_duration_by_stations_and_day`,
+  (SELECT start_station_name, end_station_name, tripduration,
+          IF (EXTRACT(DAYOFWEEK FROM starttime) =1 OR
+              EXTRACT(DAYOFWEEK FROM starttime) =7, TRUE, FALSE) AS is_weekend
+    FROM 
+        `01_nyc_bike_sharing.prediction_table`))
+ORDER BY difference_in_min ASC;
+
+# Conclusion
+SELECT COUNT(*)
+FROM (
+SELECT tripduration AS actual_duration,
+      predicted_tripduration AS predicted_duration,
+      ABS(tripduration - predicted_tripduration) AS difference_in_min
+FROM ML.PREDICT 
+  (MODEL `01_nyc_bike_sharing.trip_duration_by_stations_and_day`,
+  (SELECT start_station_name, end_station_name, tripduration,
+          IF (EXTRACT(DAYOFWEEK FROM starttime) =1 OR
+              EXTRACT(DAYOFWEEK FROM starttime) =7, TRUE, FALSE) AS is_weekend
+    FROM 
+        `01_nyc_bike_sharing.prediction_table`))
+ORDER BY difference_in_min ASC)
+WHERE difference_in_min<=15;
